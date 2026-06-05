8ddfec46f11bec6f32141fb2f5f3b52c284197d3
kirant <kiran.t@empover.com>
2026-06-05 14:54:00 +0530

    Fix casing in import path for GCLoaderScreen in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // ✅ Redux store
 export { default as store, default as persistor } from './src/redux/store/store';
 export { initLocalisation } from './src/Localisation/Localisation';
-export { default as GCLoaderScreen } from './src/SDKScreens/GCLoaderScreen';
+export { default as GCLoaderScreen } from './src/SDKscreens/GCLoaderScreen';
 export { default as EmployeeDashboardSDK } from './src/SDKscreens/EmployeeDashboardSDK';
 
 
