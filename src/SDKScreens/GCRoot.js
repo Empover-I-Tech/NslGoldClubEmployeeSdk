ffd5e7c156c5dc046e7a65096ed747494ed57adf
kirant <kiran.t@empover.com>
2026-06-10 14:13:29 +0530

    Add localization initialization in GCRoot component to enhance app localization support

--- src/SDKScreens/GCRoot.js
+++ src/SDKScreens/GCRoot.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { store } from '../redux/store/store';
 import GCNavigator from './GCNavigator';
+import { initLocalisation } from '../Localisation/Localisation';
 
 const GCRoot = (props) => {
+
+     useEffect(() => {
+        initLocalisation();
+    }, []);
+
     return (
         <Provider store={store}>
             <NavigationContainer independent={true}>
